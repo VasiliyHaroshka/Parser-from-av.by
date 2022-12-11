9c13a1c7ddae0324e42278226bda6ee89e2168db
VasiliyHaroshka <108528667+VasiliyHaroshka@users.noreply.github.com>
2022-12-11 13:04:57 +0300

    three functions

---
+++ Parcer_av.by.py
@@ -0,0 +1,85 @@
+import requests
+from bs4 import BeautifulSoup
+from icecream import ic
+import pandas as pd
+import json
+
+
+def parser() -> list:
+    """
+    Function for parcing models, links, prices and other common information about cars Toyota Land Cruiser Prado from
+    site 'av.by'. This parcer suppurts multipages.
+    Results of parcing will be shown with ascend of prices.
+    Function creates files Land Cruiser Prado.xlsx and Land Cruiser Prado.json in local directory for record of results.
+    :return: list is contained information that has parsed
+    """
+    host: str = "https://cars.av.by"
+    data: list = []
+    for page in range(1, 4):
+        print(f"Parsing page №:{page}.....")
+        url: str = f"https://cars.av.by/filter?brands[0][brand]=1181&brands[0][model]=5731&page={page}"
+        r = requests.get(url)
+        if r.status_code == 200:
+            print(f"Status code: {r.status_code}")
+            soup = BeautifulSoup(r.text, "html.parser")
+            cars = soup.find_all("div", class_="listing-item")
+
+            for car in cars:
+                try:
+                    model = car.find("div", class_="listing-item__about").find("h3").get_text().replace("\xa0", " "). \
+                        replace("\u2009", " ")
+                except Exception:
+                    model = "-"
+
+                try:
+                    link = host + car.find("div", class_="listing-item__about").find("a").get("href")
+                except Exception:
+                    link = "-"
+
+                try:
+                    price = car.find("div", class_="listing-item__prices").find("div", class_="listing-item__price"). \
+                                get_text().replace("\xa0", " ").replace("\u2009", " ") + \
+                            car.find("div", class_="listing-item__prices").find("div", class_="listing-item__priceusd"). \
+                                get_text().replace("\xa0", " ").replace("\u2009", " ")
+                except Exception:
+                    price = "-"
+
+                try:
+                    info = car.find("div", class_="listing-item__params").get_text().replace("\xa0", " "). \
+                        replace("\u2009", " ")
+                except Exception:
+                    info = "-"
+
+                data.append([model, link, price, info])
+
+    data.sort(key=lambda x: int(x[2].split()[0]))
+    ic(data)
+    print(f"{len(data)} results have been found.")
+    return data
+
+
+def to_excel(data: list) -> None:
+    """
+    Function for write list of data to .xlsx file
+    :param data: list is contained information that has parsed
+    :return: None
+    """
+    headers = ["Model", "Link", "Price", "Common information"]
+    df = pd.DataFrame(data, columns=headers)
+    df.index += 1
+    df.to_excel("Land Cruiser Prado.xlsx")
+
+
+def to_json(data: list) -> None:
+    """
+    Function for write list of data to .json file
+    :param data: list is contained information that has parsed
+    :return: None
+    """
+    with open("Land Cruiser Prado.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=3)
+
+
+if __name__ == '__main__':
+    to_excel(parser())
+    to_json(parser())
